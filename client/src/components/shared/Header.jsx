76d7beb991209218a38deaf6dfc1635219bff497
zainsattar18 <zainsattar18@gmail.com>
2020-07-21 12:43:07 -0400

    css mobile

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -7,7 +7,7 @@ import copy from '../../images/copy.jpeg'
 export default function header(props) {
   return (
     <div className="flex items-center justify-between">
-      <div className="inline-block ml-4 mt-2">
+      <div className="inline-block mt-3 ml-3 h-20 w-20 md:h-auto md:w-auto md:ml-4 md:mt-2">
         <Link to='/'>
           <img src={belayon} alt="Belay On Logo" width="150px" />
         </Link>
@@ -16,14 +16,14 @@ export default function header(props) {
         <div>
 
 
-          <div className="inline-block mb-1 rounded-full bg-gray-300 pr-6 h-20 w-auto leading-loose text-xl mr-4">
+          <div className="inline-block mb-6 rounded-full bg-gray-300 pr-1 h-10 w-48 md:pr-5 md:h-16 md:w-auto md:leading-10 md:text-xl md:bg-orange-300 md:mr-3">
             <img className="rounded-full float-left h-full" src={copy} alt="user" />
-            
-            <span className="ml-3">{props.currentUser.username}</span>
-            <button onClick={props.handleLogout} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-4 rounded-full ml-2">
+            <div className="flex py-2 md:pt-3">
+            <span className="ml-1 text-sm md:ml-3 md:text-lg">{props.currentUser.username}</span>
+            <button onClick={props.handleLogout} className="bg-blue-500 hover:bg-blue-700 text-white text-small font:medium py-0 px-1 rounded-full ml-1 md:ml-2 md:font-bold">
                 SignOut
             </button>
-              
+             </div> 
           </div>
         </div>
 
